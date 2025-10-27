e9b82806c72b1744f673ef57162724bc3d926d2b
Srihari Raman <raman.sr@northeastern.edu>
2025-10-27 13:12:51 -0400

    Deleted test MCPs

--- servers/census_server/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Census Bureau MCP server package."""
-
-from .server import app
-
-__all__ = ["app"]
--- servers/mbta_server/__init__.py
+++
@@ -1,5 +0,0 @@
-"""MBTA MCP server package."""
-
-from .server import app
-
-__all__ = ["app"]
